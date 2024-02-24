eb51761311c56728a90a6c6f636b1e701686aad7
Rahul <bhaleraor17@gmail.com>
2024-02-24 13:30:42 +0530

    button added

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div >
-
+      <Button />
 
     </div>
   );
---
+++ qtify/src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Button() {
+   return <Button>Give Feedback</Button>;
+}
+
+export default Button;
